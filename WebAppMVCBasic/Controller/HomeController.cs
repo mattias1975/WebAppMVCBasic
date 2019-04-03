47ef96b31592d95cee9fb462a886579caedf4362
utvecklare.mattias@gmail.com <utvecklare.mattias@gmail.com>
2019-04-03 10:57:52 +0200

    new

---
+++ WebAppMVCBasic/Controller/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppMVCBasic
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+                
+        
+            public IActionResult Kontakt()
+            {
+                return View();
+            }
+
+
+        public IActionResult sledge()
+
+        {
+            return View();
+            }
+        }
+}
