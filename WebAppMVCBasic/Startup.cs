4e56e9a34d681466a9ceb0b35d191a575fc719dc
utvecklare.mattias@gmail.com <utvecklare.mattias@gmail.com>
2019-04-09 16:39:21 +0200

    new

--- WebAppMVCBasic/Startup.cs
+++ WebAppMVCBasic/Startup.cs
@@ -14,26 +14,26 @@ namespace WebAppMVCBasic
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-    
-     
-      
 
-            public void ConfigureServices(IServiceCollection services)
+
+
+
+        public void ConfigureServices(IServiceCollection services)
 
         {
             services.AddDistributedMemoryCache();
 
-          
-                services.AddSession(options =>
-                {
-                    options.Cookie.Name = ".AdventureWorks.Session";
-                    options.IdleTimeout = TimeSpan.FromMinutes(10);
-                    options.Cookie.IsEssential = true;
-                });
-         
+
+            services.AddSession(options =>
+            {
+                options.Cookie.Name = ".AdventureWorks.Session";
+                options.IdleTimeout = TimeSpan.FromMinutes(10);
+                options.Cookie.IsEssential = true;
+            });
+
             services.AddSession();
             services.AddMvc();
-           
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,7 +50,12 @@ namespace WebAppMVCBasic
 
             app.UseMvc(routes =>
             {
-                routes.MapRoute("Think", "{controller=Home}/{action=sledge}/{id?}");
+                routes.MapRoute(
+                    name: "fever_route",
+                    template: "feber",
+                    defaults: new { controller = "Home", action = "Fever" });
+
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
